15d627c52356ea4d77b97917ee2dbb8be1acc46d
ashowkat <aleezahshowkat@gmail.com>
2025-07-15 18:10:33 -0400

    Added contact form

--- todo.js
+++ todo.js
@@ -1,19 +1,54 @@
 const button = document.getElementById("addTaskBtn");
 const input = document.getElementById("task");
 const list = document.getElementById("list");
-button.addEventListener("click", addTask(input))
-input.addEventListener("keydown", function(event){
-    if (event.key == "Enter")
-        addTask(input);
-})
+//let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-function addTask(input){
-    if (input !== ""){
+button.addEventListener("click", () => addTask(input.value));
+input.addEventListener("keydown", function(event){
+    if (event.key === "Enter")
+        addTask(input.value);
+});
+
+function addTask(string){
+    //tasks.push(string);
+    //localStorage.setItem("tasks", JSON.stringify(tasks));
+    const taskItem = document.createElement("li");
+    taskItem.textContent = string + "      ";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.onclick = function(){
+        list.removeChild(taskItem);
+        //tasks.splice(index, 1);
+        //localStorage.setItem("tasks", JSON.stringify(tasks));
+        //loadTasks();
+    };
+    list.appendChild(taskItem);
+    taskItem.appendChild(deleteBtn);
+    input.value = "";
+}
+/*
+function loadTasks(){
+    list.innerHTML = "";
+    tasks.forEach((taskText, index) => {
         const taskItem = document.createElement("li");
-        taskItem.textContent = input;
+        taskItem.textContent = taskText + "      ";
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.onclick = function(){
+            tasks.splice(index, 1);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            loadTasks();
+        };
+        list.appendChild(taskItem);
+        taskItem.appendChild(deleteBtn);
+    });
     }
 
-    const 
 
-    list.appendChild(taskItem);
-}
+
+window.onload = () => {
+    loadTasks();
+};
+*/
