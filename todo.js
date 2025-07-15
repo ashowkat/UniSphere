c0b4c7d1e3c3896a804325051797f21728cf9334
ashowkat <aleezahshowkat@gmail.com>
2025-07-15 16:25:31 -0400

    Added contact form

---
+++ todo.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("addTaskBtn");
+const input = document.getElementById("task");
+const list = document.getElementById("list");
+button.addEventListener("click", addTask(input))
+input.addEventListener("keydown", function(event){
+    if (event.key == "Enter")
+        addTask(input);
+})
+
+function addTask(input){
+    if (input !== ""){
+        const taskItem = document.createElement("li");
+        taskItem.textContent = input;
+    }
+
+    list.appendChild(taskItem);
+}
