cae610eb1f4e2c8d2b2c30449e469d9d233010f8
ashowkat <aleezahshowkat@gmail.com>
2025-07-29 15:40:52 -0400

    Newest changes 7/25

--- todo.js
+++ todo.js
@@ -7,6 +7,13 @@ const list = document.getElementById("list");
 list.style.listStyleType = 'none';
 let tasks = [];
 let taskStack = [];
+let checkBools = [];
+
+loadTasks();
+document.addEventListener('visibilitychange', function () {
+    if (!document.hidden)
+        loadTasks();
+})
 
 button.addEventListener("click", () => addTask(input.value));
 input.addEventListener("keydown", function (event) {
@@ -14,7 +21,7 @@ input.addEventListener("keydown", function (event) {
         addTask(input.value);
 });
 
-function addTask(string) {
+function addTask(string, bool = undefined) {
     if (string.trim() != "") {
         const taskItem = document.createElement('li');
 
@@ -24,6 +31,7 @@ function addTask(string) {
         deleteBtn.onclick = function () {
             list.removeChild(taskItem);
             taskStack.push(taskItem);
+            checkBools.push(checkbox.checked);
             saveTasks();
         };
 
@@ -37,7 +45,19 @@ function addTask(string) {
             else {
                 taskItem.style.textDecoration = 'none';
             }
+            saveTasks();
         })
+
+        if (bool !== undefined) {
+            if (bool) {
+                checkbox.checked = true;
+                taskItem.style.textDecoration = 'line-through';
+            }
+            else {
+                checkbox.checked = false;
+                taskItem.style.textDecoration = 'none';
+            }
+        }
 
         const taskText = document.createElement('span');
         taskText.textContent = string + '   ';
@@ -53,38 +73,49 @@ function addTask(string) {
 
 function saveTasks() {
     tasks = [];
+    checkBools = [];
     list.querySelectorAll('li').forEach(function (item) {
-        tasks.push(item.firstChild.nextSibling.textContent);
+        tasks.push(item.firstElementChild.nextElementSibling.textContent.trim());
+        checkBools.push(item.firstElementChild.checked);
     })
 
     localStorage.setItem("tasks", JSON.stringify(tasks));
     localStorage.setItem("taskStack", JSON.stringify(taskStack));
+    localStorage.setItem("checkBools", JSON.stringify(checkBools));
 }
 
 function loadTasks() {
     tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     taskStack = JSON.parse(localStorage.getItem("taskStack")) || [];
-    tasks.forEach(function (item) {
-        addTask(item);
+    let checkBoolsOne = JSON.parse(localStorage.getItem("checkBools")) || [];
+    tasks.forEach(function (item, i) {
+        if (checkBoolsOne[i] === undefined) {
+            addTask(item.trim(), false)
+        }
+        else {
+            addTask(item.trim(), checkBoolsOne[i]);
+        }
     })
 }
-
-loadTasks();
 
 //undo btn
 const undoBtn = document.getElementById("undoBtn");
 undoBtn.addEventListener('click', function () {
     if (taskStack.length > 0) {
-        addTask(taskStack.pop().firstChild.nextSibling.textContent);
+        obj = taskStack.pop()
+        addTask(obj.firstElementChild.nextElementSibling.textContent, obj.firstElementChild.checked);
+        checkBools.pop();
     }
 })
 
 //clear btn
 const clearBtn = document.getElementById("clearBtn");
 clearBtn.addEventListener('click', function () {
-    while (list.firstChild) {
-        taskStack.push(list.firstChild.nextSibling);
-        list.removeChild(list.firstChild).nextSibling;
-        saveTasks();
+    while (list.firstElementChild) {
+        let li = list.firstElementChild
+        taskStack.push(li);
+        checkBools.push(li.firstElementChild.checked);
+        list.removeChild(li);
     }
+    saveTasks();
 })
